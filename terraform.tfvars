e6d238a1eed67d353923dddd578a56df7d89cac1
Ubuntu <ubuntu@ip-172-31-10-96.ap-northeast-1.compute.internal>
2022-07-18 08:48:33 +0000

    files added

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+region = "ap-northeast-1"
+clustername = "kafkademo"
+kafkaversion = "3.2.0"
+numnodes = 3
+brokerinst = "kafka.m5.4xlarge"
+volsize = 100
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+description = "enter region"
+}
+variable "clustername" {
+description = "enter clustername"
+}
+variable "kafkaversion" {
+description = "enter kafaka version"
+}
+variable "numnodes" {
+description = "enter number of broker nodes"
+}
+variable "brokerinst" {
+description = "enter instance type of broker nodes"
+}
+variable "volsize" {
+description = "enter volume size"
+}
+
+
